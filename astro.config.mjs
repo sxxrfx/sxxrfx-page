29305f2c871163303449900671fc09360e2628b3
Sagar Negi <sxxrfx@gmail.com>
2024-06-21 19:04:16 +0530

    feat:change base to /sxxrfx-page

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sxxrfx.github.io",
-  // base: "/pages",
+  base: "/sxxrfx-page",
   integrations: [tailwind()],
 });
